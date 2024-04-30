d776efd59dca3b5a495e307dfdd7999630b053cb
rn25-long-app <bhlong3112@gmail.com>
2024-04-30 14:24:54 +0700

    config REDUX

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+  users: [
+    { id: 1, name: "teo" },
+    { id: 2, name: "ti" },
+  ],
+};
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
